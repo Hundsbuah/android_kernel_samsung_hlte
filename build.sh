bdd0a185cd1ac5edb2d335c1558e62f43ad06a5b
Christian Lohwasser <christian.lohwasser@gmx.de>
2015-02-07 10:15:51 +0100

    change build.sh script because of commit f3ec6eb1b2129c4e3e62eec03f3714bc98d5b700

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ mkdir out
 ~/Daten/KERNEL/boot_tools/x64/dtbTool -s 2048 -o arch/arm/boot/dt.img -p scripts/dtc/ arch/arm/boot/
 cp arch/arm/boot/zImage out/zImage
 cp arch/arm/boot/dt.img out/dt.img
-~/Daten/KERNEL/boot_tools/x64/mkbootfs ~/Daten/KERNEL/ramdisk/CM12 | gzip > out/ramdisk.gz
+~/Daten/KERNEL/boot_tools/x64/mkbootfs ramdisk/ | gzip > out/ramdisk.gz
 ~/Daten/KERNEL/boot_tools/x64/mkbootimg --kernel out/zImage --ramdisk out/ramdisk.gz --dt out/dt.img --cmdline "console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F androidboot.bootdevice=msm_sdcc.1 androidboot.selinux=permissive" --base 0x26fff00 --ramdisk_offset 0x2900000 --tags_offset 0x2700000 --pagesize 2048 -o out/boot.img
 
 #console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F androidboot.bootdevice=msm_sdcc.1 androidboot.selinux=permissive
