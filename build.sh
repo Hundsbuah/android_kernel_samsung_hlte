1480417fe2b81bba70bf96454079d7813917f64e
Christian Lohwasser <christian.lohwasser@gmx.de>
2015-02-08 12:32:43 +0100

    extend build.sh script for build automating

--- build.sh
+++ build.sh
@@ -14,11 +14,25 @@ cp `find ./ | grep .ko$` modules.order mod/
 rm -rf out
 mkdir out
 
+find /home/lowi/Daten/KERNEL/flashable_zip/ -name "*-kernel-*.zip" -exec cp {} /home/lowi/Daten/KERNEL/git/android_kernel_samsung_hlte/out \;
+
 ~/Daten/KERNEL/boot_tools/x64/dtbTool -s 2048 -o arch/arm/boot/dt.img -p scripts/dtc/ arch/arm/boot/
 cp arch/arm/boot/zImage out/zImage
 cp arch/arm/boot/dt.img out/dt.img
 ~/Daten/KERNEL/boot_tools/x64/mkbootfs ramdisk/ | gzip > out/ramdisk.gz
 ~/Daten/KERNEL/boot_tools/x64/mkbootimg --kernel out/zImage --ramdisk out/ramdisk.gz --dt out/dt.img --cmdline "console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F androidboot.bootdevice=msm_sdcc.1 androidboot.selinux=permissive" --base 0x26fff00 --ramdisk_offset 0x2900000 --tags_offset 0x2700000 --pagesize 2048 -o out/boot.img
 
-#console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F androidboot.bootdevice=msm_sdcc.1 androidboot.selinux=permissive
-#console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F androidboot.bootdevice=msm_sdcc.1 androidboot.selinux=permissive
+cd out
+find . -name "*-kernel-*.zip" -exec zip -d {} boot.img \;
+find . -name "*-kernel-*.zip" -exec zip -u {} boot.img \;
+cd ..
+
+if [ $# -eq 0 ]
+  then
+    echo "\n\n  no version specified for flashable zip\n  use sh build.sh -33 for setting version to 33!\n  overwriting archive:"
+    echo "  --> `find out/ -name "*-kernel-*.zip"`\n\n"
+else
+  flashable_zip_orig=`find out/ -name "*-kernel-*.zip"`
+  flashable_zip_new=$(echo $flashable_zip_orig | sed "s/-001/$1/g")
+  mv $flashable_zip_orig $flashable_zip_new
+fi
